0204c5d1eb24fa7b169cba61a2cba3c2204b935f
Lindsay <lindsay.chen@yale.edu>
2021-06-26 08:39:55 +0800

    commit tree 8bea4a8fe9f2d98b628ccd28af4595f15adbce13

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,23 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        self.first_name + " " + self.last_name
+    end
   
+    def list_roles
+        list = []
+        self.characters.each do |cha|
+            first_half = cha.name
+            last_half = cha.show.name
+            whole = first_half + " - " + last_half
+            list << whole
+        end
+        list
+    end
+
+
+       
+
 end
